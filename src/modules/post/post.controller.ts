df38b8be1d5fd8a24d9e9f262642fa3e43547913
sayed725 <ssayed72533@gmail.com>
2026-01-11 12:06:43 +0600

    added global error middleware for  update post

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -106,7 +106,7 @@ const getMyPosts = async (req: Request, res: Response) => {
     }
 }
 
-const updatePost = async (req: Request, res: Response) => {
+const updatePost = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const user = req.user;
         if (!user) {
@@ -119,11 +119,7 @@ const updatePost = async (req: Request, res: Response) => {
         const result = await postService.updatePost(postId as string, req.body, user.id, isAdmin);
         res.status(200).json(result)
     } catch (e) {
-        const errorMessage = (e instanceof Error) ? e.message : "Post update failed!"
-        res.status(400).json({
-            error: errorMessage,
-            details: e
-        })
+      next(e)
     }
 }
 
